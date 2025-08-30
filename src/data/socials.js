3c353dcbcddf2feb45397e79e1612cee28717755
joaiser <aitor.v@hotmail.com>
2025-08-30 20:15:08 +0200

    Añadidos enlaces nav, arreglo degradado footer, lógica de enlaces centralizada

---
+++ src/data/socials.js
@@ -0,0 +1,12 @@
+export const socials = [
+  {
+    label: "GitHub",
+    href: "https://github.com/Joaiser",
+    icon: "github",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/aitor-vicent-2534392b7/",
+    icon: "linkedin",
+  },
+];
